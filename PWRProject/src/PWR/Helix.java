e280fc48fa03ee37f6959f326ab293116fcf89c1
Kieren Foenander <kieren.foenander@cqumail.com>
2020-10-31 10:07:00 +1000

    Helix complete

---
+++ PWRProject/src/PWR/Helix.java
@@ -0,0 +1,37 @@
+/*
+ * /*
+Programmer: Kieren Foenander S012141776
+Course: Object Oriented Programming COIT11134
+Assignment 1
+ */
+
+package PWR;
+
+/**
+ *
+ * @author black
+ */
+public class Helix extends Core{
+    
+    String difficulty;
+    
+    public Helix (String teamName, int partNumber, String difficulty){
+        super(teamName, partNumber);
+        this.difficulty = difficulty;
+        
+        
+    }
+    
+    public String getDifficulty(){
+        return difficulty;
+        
+    }
+    
+    public void setDifficulty(String difficulty){
+        this.difficulty = difficulty;
+        
+    }
+    
+    
+
+}
